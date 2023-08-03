e49a2b561e9e935b1977fe66739dc34c58abc50b
pooria <pooria.shariatzadeh@gmail.com>
2023-08-03 14:13:13 +0330

    fix bugs and add omax

---
+++ CustomerClub/CheckCustomerClubBalanceDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopTis.TouristPanel.Public.Contracts.CustomerClub
+{
+	public class CheckCustomerClubBalanceDto
+	{
+        public string MobilePhone { get; set; }
+        public string InvoiceAmount { get; set; }
+    }
+}
